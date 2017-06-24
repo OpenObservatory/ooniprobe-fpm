92ca80c35300fe9a44e4f58dfe644eb3c19a1942
Arturo Filastò <arturo@filasto.net>
2017-06-25 01:22:18 +0200

    Add xcodeproject for ooniprobe launcher

---
+++ launcher/macos/ooniprobe-launcher/ooniprobe-launcherTests/ooniprobe_launcherTests.m
@@ -0,0 +1,39 @@
+//
+//  ooniprobe_launcherTests.m
+//  ooniprobe-launcherTests
+//
+//  Created by x on 24/06/2017.
+//  Copyright © 2017 openobservatory. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ooniprobe_launcherTests : XCTestCase
+
+@end
+
+@implementation ooniprobe_launcherTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
